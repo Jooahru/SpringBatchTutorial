6591030c728a4491358cd4c1d33c6dcd19b7350e
WILL <dndlf333@naver.com>
2023-05-31 08:41:55 +0900

    [리팩토링] 주석 제거 및 코드 정렬
     - 실행 : --job.name=multipleStepJob

--- src/main/java/com/example/SpringBatchTutorial/job/MultipleStep/MultipleStepJobConfig.java
+++ src/main/java/com/example/SpringBatchTutorial/job/MultipleStep/MultipleStepJobConfig.java
@@ -13,77 +13,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * desc: 다중 step을 사용하기 및 step to step 데이터 전달
- * run param: --job.name=multipleStepJob
- */
 @Configuration
 @RequiredArgsConstructor
 public class MultipleStepJobConfig {
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
 
-    @Bean
-    public Job multipleStepJob(Step multipleStep1, Step multipleStep2, Step multipleStep3) {
-        return jobBuilderFactory.get("multipleStepJob")
-                .incrementer(new RunIdIncrementer())
-                .start(multipleStep1)
-                .next(multipleStep2)
-                .next(multipleStep3)
-                .build();
-    }
+	@Bean
+	public Job multipleStepJob(Step multipleStep1, Step multipleStep2, Step multipleStep3) {
+		return jobBuilderFactory.get("multipleStepJob").incrementer(new RunIdIncrementer()).start(multipleStep1)
+				.next(multipleStep2).next(multipleStep3).build();
+	}
 
-    @JobScope
-    @Bean
-    public Step multipleStep1() {
-        return stepBuilderFactory.get("multipleStep1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
+	@JobScope
+	@Bean
+	public Step multipleStep1() {
+		return stepBuilderFactory.get("multipleStep1").tasklet((contribution, chunkContext) -> {
+			System.out.println("step1");
+			return RepeatStatus.FINISHED;
+		}).build();
+	}
 
-    @JobScope
-    @Bean
-    public Step multipleStep2() {
-        return stepBuilderFactory.get("multipleStep2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2");
+	@JobScope
+	@Bean
+	public Step multipleStep2() {
+		return stepBuilderFactory.get("multipleStep2").tasklet((contribution, chunkContext) -> {
+			System.out.println("step2");
 
-                    ExecutionContext executionContext = chunkContext
-                            .getStepContext()
-                            .getStepExecution()
-                            .getJobExecution()
-                            .getExecutionContext();
+			ExecutionContext executionContext = chunkContext.getStepContext().getStepExecution().getJobExecution()
+					.getExecutionContext();
 
-                    executionContext.put("someKey", "hello!!");
+			executionContext.put("someKey", "hello!!");
 
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
+			return RepeatStatus.FINISHED;
+		}).build();
+	}
 
-    @JobScope
-    @Bean
-    public Step multipleStep3() {
-        return stepBuilderFactory.get("multipleStep3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step3");
+	@JobScope
+	@Bean
+	public Step multipleStep3() {
+		return stepBuilderFactory.get("multipleStep3").tasklet((contribution, chunkContext) -> {
+			System.out.println("step3");
 
-                    ExecutionContext executionContext = chunkContext
-                            .getStepContext()
-                            .getStepExecution()
-                            .getJobExecution()
-                            .getExecutionContext();
+			ExecutionContext executionContext = chunkContext.getStepContext().getStepExecution().getJobExecution()
+					.getExecutionContext();
 
-                    System.out.println(executionContext.get("someKey"));
+			System.out.println(executionContext.get("someKey"));
 
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
+			return RepeatStatus.FINISHED;
+		}).build();
+	}
 }
